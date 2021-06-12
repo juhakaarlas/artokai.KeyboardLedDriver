110108f83fe0aef79025db520cb4178ca26d75cb
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2021-06-12 23:08:20 +0200

    Added basic tests for Spectral

---
+++ KeyboardLedDriver.Corsair.Tests/SpectralDriverTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using KeyboardLedDriver.Common;
+using KeyboardLedDriver.Generic;
+using Spectral;
+using Xunit;
+
+namespace KeyboardLedDriver.Corsair.Tests
+{
+    public class SpectralFixture : IDisposable
+    {
+        public GenericLedController Controller { get; private set; }
+
+        public SpectralFixture()
+        {
+            Controller = new GenericLedController();
+            Controller.Initialize();
+        }
+
+        public void Dispose()
+        {
+            Controller.ShutDown();
+        }
+    }
+
+    public class SpectralDriverTests :IClassFixture<SpectralFixture>
+    {
+        private SpectralFixture _fixture;
+
+        public SpectralDriverTests(SpectralFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void TrySetColorScheme()
+        {
+           _fixture.Controller.SetColorScheme(new ColorScheme { R = 255, G = 255, B = 255 }, false);
+        }
+
+        [Fact]
+        public void TrySetErrorColorScheme()
+        {
+            _fixture.Controller.SetColorScheme(new ColorScheme { R = 255, G = 255, B = 255 }, true);
+        }
+    }
+}
--- KeyboardLedDriver.Spectral/SpectralLedController.cs
+++ KeyboardLedDriver.Spectral/SpectralLedController.cs
@@ -1,37 +1,66 @@
 ﻿using System;
+using System.Drawing;
 using KeyboardLedDriver.Common;
 using Spectral;
 
-namespace KeyboardLedDriver.Spectral
+namespace KeyboardLedDriver.Generic
 {
-    public class SpectralLedController : ILedController
+    public class GenericLedController : ILedController
     {
+        private readonly LedName[] ALERT_KEYS =
+        {
+            LedName.Escape,
+            LedName.F1,
+            LedName.F2,
+            LedName.F3,
+            LedName.F4,
+            LedName.F5,
+            LedName.F6,
+            LedName.F7,
+            LedName.F8,
+            LedName.F9,
+            LedName.F10,
+            LedName.F11,
+            LedName.F12,
+            LedName.PrintScreen,
+            LedName.ScrollLock,
+            LedName.Pause
+        };
         public bool CurrentAlertState { get; }
         public ColorScheme CurrentColorScheme { get; }
         
         public bool IsInitialized { get; private set; }
 
-        private SpectralLedController _controller;
-
-        public SpectralLedController()
-        {
-            _controller = new SpectralLedController();
-        }
-        
         public bool Initialize()
         {
-            IsInitialized = _controller.Initialize();
-            return IsInitialized;
+            IsInitialized = Led.Initialize();
+            return IsInitialized && (Led.CorsairIsEnabled() || Led.LogitechIsEnabled());
         }
 
         public void SetColorScheme(ColorScheme scheme, bool showAlert)
         {
-            throw new NotImplementedException();
+            if (!IsInitialized) return;
+
+            LedName[] ledsToSet;
+            Color color;
+
+            if (!showAlert)
+            {
+                ledsToSet = new LedName[1] { LedName.Escape };
+                color = Color.Chartreuse;
+            }
+            else
+            {
+                ledsToSet = ALERT_KEYS;
+                color = Color.OrangeRed;
+            }
+
+            Led.SetColorForLeds(ledsToSet, color);
         }
 
         public void ShutDown()
         {
-            throw new NotImplementedException();
+            Led.Shutdown();
         }
 
         public void ToggleAlert(bool alertState)
